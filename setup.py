93649642d3ccb548549e9f3d0ad0f056f3d198e1
Kevin Le <kle@liftoff.io>
2024-09-16 13:14:11 -0700

    update pipes.quote reference

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@
 #  the file COPYING, distributed as part of this software.
 # -----------------------------------------------------------------------------
 import os
-import pipes
+import shlex
+from distutils import log
 from subprocess import check_call
 
-from distutils import log
 from setuptools import setup
 from setuptools.command.develop import develop
 
@@ -17,7 +17,7 @@ import versioneer
 
 def sh(cmd):
     """Run a command, echoing what command is to be run"""
-    log.info("Running command %s" % " ".join(map(pipes.quote, cmd)))
+    log.info("Running command %s" % " ".join(map(shlex.quote, cmd)))
     check_call(cmd)
 
 
